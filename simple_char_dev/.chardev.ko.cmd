b8e3de847a77586fa9838b7222a69470c9a2ff18
Zach Brown <brownzach125@gmail.com>
2017-11-09 19:22:37 +0000

    Initial

---
+++ simple_char_dev/.chardev.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vagrant/module/simple_char_dev/chardev.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-135-generic/scripts/module-common.lds --build-id  -o /home/vagrant/module/simple_char_dev/chardev.ko /home/vagrant/module/simple_char_dev/chardev.o /home/vagrant/module/simple_char_dev/chardev.mod.o
---
+++ simple_char_dev/.tmp_versions/chardev.mod
@@ -0,0 +1,2 @@
+/home/vagrant/module/simple_char_dev/chardev.ko
+/home/vagrant/module/simple_char_dev/chardev.o
